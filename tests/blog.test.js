d6859dd114f667e5bb9e5a878c753b95fbea6fe8
Martin Häggblom <martin.haggblom@gmail.com>
2024-10-17 10:53:10 +0300

    submit 5.17: Blog List End To End Testing, step 1

---
+++ tests/blog.test.js
@@ -0,0 +1,21 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form shown', async ({ page }) => {
+    const loginTitle = await page.locator('text=Log in to application')
+    await expect(loginTitle).toBeVisible()
+
+    const usernameInput = await page.locator('input[name="Username"]')
+    await expect(usernameInput).toBeVisible()
+
+    const passwordInput = await page.locator('input[name="Password"]')
+    await expect(passwordInput).toBeVisible()
+
+    const loginButton = await page.locator('button[type="submit"]')
+    await expect(loginButton).toBeVisible()
+  })
+})
