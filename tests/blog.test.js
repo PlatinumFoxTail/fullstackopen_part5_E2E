7b8e7afff59e287db34186889b02905b0baf0225
Martin Häggblom <martin.haggblom@gmail.com>
2024-10-17 11:54:32 +0300

    submit 5.18: Blog List End To End Testing, step 2

--- tests/blog.test.js
+++ tests/blog.test.js
@@ -1,7 +1,16 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+        data: {
+          name: 'Matti Luukkainen',
+          username: 'mluukkai',
+          password: 'salainen'
+        }
+      })
+    
     await page.goto('http://localhost:5173')
   })
 
@@ -18,4 +27,27 @@ describe('Blog app', () => {
     const loginButton = await page.locator('button[type="submit"]')
     await expect(loginButton).toBeVisible()
   })
+
+  describe('when logged in', () => {
+      test('succeeds with correct credentials', async ({ page }) => {
+          await page.fill('input[name="Username"]', 'mluukkai')
+          await page.fill('input[name="Password"]', 'salainen')
+          await page.click('button[type="submit"]')
+
+          const loggedInUser = await page.locator('text=blogs')
+          await expect(loggedInUser).toBeVisible()
+      })
+
+      test('fails with wrong credentials', async ({ page }) => {
+          await page.fill('input[name="Username"]', 'mluu')
+          await page.fill('input[name="Password"]', 'sala')
+          await page.click('button[type="submit"]')
+    
+          const errorMessage = await page.locator('text=wrong credentials')
+          await expect(errorMessage).toBeVisible()
+
+          const loginForm = await page.locator('text=Log in to application')
+          await expect(loginForm).toBeVisible()
+      })
+    })
 })
